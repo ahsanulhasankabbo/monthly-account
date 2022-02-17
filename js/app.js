18f5a539958fcb8894c4ab8c3e1d8c7e4b9e55f5
Ahsanul Hasan Kabbo <kabboahsanulhasan@gmail.com>
2022-02-17 17:53:03 +0600

    error handle solved

--- js/app.js
+++ js/app.js
@@ -4,9 +4,11 @@ function getInput(inutId){
     const inputCost = inputField.value; 
     return inputCost;
 }
-function updateBalance(number1 , number2){
-    const sub = number1 - number2;
-    return sub;
+// get remaining amout 
+function updateBalance(existanceAmount , updateAmount){
+    const total = existanceAmount - updateAmount;
+
+    return total;
 }
 // calculate button 
 document.getElementById("calculate-button").addEventListener("click",function(){
@@ -14,21 +16,52 @@ document.getElementById("calculate-button").addEventListener("click",function(){
     const foodCost = getInput("food-input");
     const rentCost = getInput("rent-input");
     const clothesCost = getInput("clothes-input");
-    
-    const totalCost = parseFloat(foodCost) + parseFloat(rentCost) + parseFloat(clothesCost);
-    
-    const totalCostInput = document.getElementById("total-cost");
-    totalCostInput.innerText = totalCost;
-
-    // update balance 
     const incomeCost = getInput("income-input");
 
+    const totalCostInput = document.getElementById("total-cost");
     const totalBalance = document.getElementById("total-balance");
-    const totalBalanceSave = totalBalance.innerText;
-    // const newBalance = incomeCost - totalCost;
-    // totalBalance.innerText = newBalance;
+    const expensesWarn =  document.getElementById("empty-calculate");
+
+    if(foodCost>0 && rentCost>0 && clothesCost>0 && incomeCost>0){
+        const totalCost = parseFloat(foodCost) + parseFloat(rentCost) + parseFloat(clothesCost);
+        
+        totalCostInput.innerText = totalCost;
+
+    // update balance 
+    
+
     const newBalance = updateBalance(incomeCost,totalCost);
-    totalBalance.innerText = newBalance;
+            // when cost is more than income solve 
+            if(incomeCost>totalCost){
+                totalBalance.innerText = newBalance;
+                expensesWarn.innerText = "";
+            }
+            else{
+                expensesWarn.innerText = "Expenses is more then income.";
+                totalCostInput.innerText = 0 ;
+                totalBalance.innerText = incomeCost;
+            }
+    }
+    else if (incomeCost<0){
+        expensesWarn.innerText = "Please,enter a possitive number";
+        totalCostInput.innerText = 0 ;
+        totalBalance.innerText = 0 ;
+    }
+    else if (isNaN(incomeCost)){
+        expensesWarn.innerText = "Please,enter a valid number";
+        totalCostInput.innerText = 0 ;
+        totalBalance.innerText = 0 ;
+    }
+    else if(foodCost<0 || rentCost<0 || clothesCost<0){
+        expensesWarn.innerText = "Please,enter a possitive number";
+        totalCostInput.innerText = 0 ;
+        totalBalance.innerText = incomeCost;
+    }
+    else{
+        expensesWarn.innerText = "Please,enter a valid number";
+        totalCostInput.innerText = 0 ;
+        totalBalance.innerText = incomeCost;
+    }   
 })
 
 // save button 
@@ -37,22 +70,40 @@ document.getElementById("save-button").addEventListener("click",function(){
     const saveCost = getInput("save-input");
     const saveNumver = parseFloat(saveCost);
     const incomeCost = getInput("income-input");
-    
-    const savingAmount = (saveNumver * incomeCost ) / 100 ;
-
     const saveTotal = document.getElementById("save-amount");
-    const saveTotalNumber = saveTotal.innerText;
-    saveTotal.innerText = savingAmount;
-
-    // update remaining balance 
-    const remainingBalance = document.getElementById("remaining-balance");
-    const remainingBalanceNumber = remainingBalance.innerText;
-
-    const totalBalance = document.getElementById("total-balance");
-    const totalBalanceSave = totalBalance.innerText;
+    const saveWarn = document.getElementById("empty-save");
     
-    // const remainingAmount = totalBalanceSave - savingAmount;
-    // remainingBalance.innerText = remainingAmount;
-    const remainingAmount = updateBalance(totalBalanceSave,savingAmount);
-    remainingBalance.innerText = remainingAmount;
+    if(saveNumver>0 && incomeCost>0 ){
+        const savingAmount = (saveNumver * incomeCost ) / 100 ;
+
+        saveTotal.innerText = savingAmount;
+
+         // update remaining balance 
+        const remainingBalance = document.getElementById("remaining-balance");
+
+        const totalBalance = document.getElementById("total-balance");
+        const totalBalanceSave = totalBalance.innerText;
+
+        
+
+        const remainingAmount = updateBalance(totalBalanceSave,savingAmount);
+        // when saving is more than balance solve 
+        if(totalBalanceSave>=savingAmount){
+            remainingBalance.innerText = remainingAmount;
+            saveWarn.innerText = "";
+        }
+         else{
+             saveWarn.innerText = "Saving is more than Balance";
+             saveTotal.innerText = 0 ;
+             remainingBalance.innerText = totalBalanceSave;
+        }   
+    }
+    else if(saveNumver<0 || incomeCost<0){
+        saveWarn.innerText = "please,enter a possitive number";
+        saveTotal.innerText = 0 ;
+    }
+    else{
+        saveWarn.innerText = "please,enter a valid number";
+        saveTotal.innerText = 0 ;
+    }
 })
